df9575e3d592309ddb5fe38744182089d37e4cab
Michael Spencer <michaelhspencer@gmail.com>
2010-01-21 22:59:00 -0500

    First round of changes from CJ and Scott (page alignment, sidebar lightbox, minimal footer, justification, etc.)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'home'
-  map.page ':page', :controller => 'page', :action => 'show', :page => /about|ataglance|citizenship|civilservant|portfolio|commercial|financing|history|hotelresort|institutional|leadership|lowcost|media|partners|residential|services|urbanrenewal|models|lowcost|civilservant/
+  map.page ':page', :controller => 'page', :action => 'show', :page => /about|citizenship|civilservant|portfolio|commercial|financing|history|hotelresort|institutional|leadership|lowcost|media|partners|residential|services|urbanrenewal|civil|lowcost|civilservant/
   map.contact '/contact/:action', :controller => 'contact', :action => 'index' 
   map.contact '/contact/:action', :controller => 'contact', :action => 'send_mail' 
   #  map.connect ':controller/:action/:id' 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery(window).load(function(){
+	/*sidebar image slider*/	
+	if (jQuery('#sidebar_image img').length > 0 )
+	{	
+		jQuery('#sidebar_image img').not('.current_img').css({display:"none"});
+		// set the automatic image rotation, number is time between transitions in miliseconds
+		interval = setInterval(function() { k_fader("#sidebar_image img",'1'); }, 5200); 	
+	}
+});
