03fa7213b492eb06e7e56c1e974278187b44f79d
Michael Spencer <michaelhspencer@gmail.com>
2010-01-26 19:47:10 -0500

    Removed old pages and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'home'
+  map.contact '/contact/:action', :controller => 'contact'
   map.page ':page', :controller => 'page', :action => 'show', :page => /about|citizenship|civilservant|portfolio|commercial|financing|history|hotelresort|institutional|leadership|lowcost|media|partners|residential|services|urbanrenewal|civil|lowcost|civilservant/
-  map.contact '/contact/:action', :controller => 'contact', :action => 'index' 
-  map.contact '/contact/:action', :controller => 'contact', :action => 'send_mail' 
-  #  map.connect ':controller/:action/:id' 
 end
